52bc48b5149c42bef8753fd3c858dd335f97f646
rnoguer_22 <91721762+rnoguer22@users.noreply.github.com>
2022-02-21 18:22:10 +0000

    Finzalizamos el ejericicio 11 :)

--- ej11.py
+++ ej11.py
@@ -1,13 +1,26 @@
-#Primera parte del ejercicio
-
+"Primera parte del ejercicio"
 def mcd_euclides (a,b):
   #Vamos a resolver el algoritmo de euclides utilizando iteracion
-  while b =! 0:
+  while b != 0:
     # Hacemos uso de la siguiente propiedad: mcd(a,b) = mcd(a−b,b)
+    aux = b
     b = a%b
+    a = aux
     #De esta manera vamos reduciendo a hasta que este sea el mcd
+  return a
+
+"Segunda parte del ejercicio"
+def mcd_sumas_restas (a,b):
+  #Ahora tenemos un handicap; solo podemos usar sumas y restas
+  while b != 0:
+    aux = b-a
+    b -= aux
+    a = aux
     return a
 
 if __name__ == "__main__":
 
-  print (mcd_euclides(105, 70))
+  num1 = 6
+  num2 = 8
+  print ("Por el algoritmo de Euclides: {}".format(mcd_euclides(num1, num2)))
+  print ("Usando sumas y restas: ", mcd_sumas_restas(num1,num2))
