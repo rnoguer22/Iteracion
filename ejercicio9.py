def92e31c212f3039a053a61646b58bd02f1ae24
pelahumi <91721764+pelahumi@users.noreply.github.com>
2022-02-22 15:28:00 +0000

    cambios

--- ejercicio9.py
+++ ejercicio9.py
@@ -2,5 +2,11 @@ import tabulate
 
 def palabra():
   diccionario = {1 : "Patata", 2 : "Fotos", 3 : "Ancla", 4 : "Cucaracha"}
+  
+  print("Las palabras de este diccionario son: ")
+  for i in diccionario:
+    print("{}".format(diccionario[i]))
 
   
+
+palabra()
