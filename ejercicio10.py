f3c75ce45bf05dcca87f0f32b9afb57e44a31215
rnoguer_22 <91721762+rnoguer22@users.noreply.github.com>
2022-02-23 19:21:34 +0000

    Ejercicio 10 (sin terminar)

---
+++ ejercicio10.py
@@ -0,0 +1,38 @@
+class Persona:
+  #Constructor
+  def __init__(self, identidad, edad, padre, madre):
+    self.identidad = identidad
+    self.edad = edad
+    self.padre = padre
+    self.madre = madre
+
+  def lista_edad_20_30(self):
+    r = []
+    for i in range(numero de personas):
+      if 20 < self.edad < 30:
+        r.append(self.nombre)
+    return r
+
+  def numero_huerfanos(self):
+    #Lista vacia donde se almacenaran la identidad de los niños huerfanos
+    r = []
+    for i in range (1000)
+    if self.padre == 0 and self.madre == 0:
+      if self.edad < 15:
+        r.append(self.identidad)
+    return r
+  
+  def envejecer_1_año(self):
+    for i in range (numero de personas):
+      self.edad[i] += 1
+
+  def identidad_padre_jaime(self):
+    if self.identidad = "Jaime MARTIN"
+      return self.padre
+
+if __name__ == "__main__":
+
+  familias = []
+  for i in range (1000):
+    r = Persona(identidad, padre, madre, edad)
+    familias.append(r)
